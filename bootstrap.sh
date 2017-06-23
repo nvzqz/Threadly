153ccbf733d54723d2dc4ea1a8670501f55d33b0
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-06-23 12:55:08 -0400

    Create Vagrantfile and bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# A provision script for Vagrant to setup a Swift-ready development environment.
+# Author: Nikolai Vazquez (https://github.com/nvzqz)
+
+set -e
+
+sudo apt-get update
+sudo apt-get install -y git zsh clang libicu-dev
+
+sudo chsh -s /bin/zsh vagrant
+
+SWIFT_VERSION="swift-3.1.1"
+SWIFTENV_ROOT="$HOME/.swiftenv"
+
+if [ ! -d "$SWIFTENV_ROOT" ]; then
+    git clone https://github.com/kylef/swiftenv.git "$SWIFTENV_ROOT"
+    echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.zshrc
+    echo 'export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"' >> ~/.zshrc
+fi
+
+source ~/.zshrc
+
+echo "Installing $SWIFT_VERSION..."
+swiftenv install "$SWIFT_VERSION" > /dev/null 2>&1
